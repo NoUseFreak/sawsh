336bab0cd3fd1bab0ecca3521190399c67d9be28
Dries De Peuter <dries@nousefreak.be>
2018-03-22 09:23:39 +0100

    Add exec support

--- sawsh.go
+++ sawsh.go
@@ -30,25 +30,51 @@ func main() {
 	app.Action = connectAction
 	app.EnableBashCompletion = true
 
+	regionFlag := cli.StringFlag{
+		Name:  "aws-region",
+		Value: "us-east-1",
+	}
+
+	app.Flags = []cli.Flag{
+		regionFlag,
+	}
+
 	app.Commands = []cli.Command{
-		{
-			Name:   "list",
-			Usage:  "Render a list of instances",
-			Action: listAction,
-		},
 		{
 			Name:      "connect",
 			Usage:     "Search and connect to an instance",
 			ArgsUsage: "[filter]",
 			Flags: []cli.Flag{
+				regionFlag,
 				cli.BoolFlag{
 					Name:  "transparant",
 					Usage: "Connect if the ec2 query has no results",
 				},
+				cli.BoolFlag{
+					Name:  "print",
+					Usage: "Print instead of connecting",
+				},
 			},
 			Action: connectAction,
 		},
+		{
+			Name:      "exec",
+			Usage:     "Search and execute a command on multiple servers",
+			ArgsUsage: "[filter] [command]",
+			Flags: []cli.Flag{
+				regionFlag,
+			},
+			Action: execAction,
+		},
+		{
+			Name:   "list",
+			Usage:  "Render a list of instances",
+			Action: listAction,
+		},
 	}
+
+	sort.Sort(cli.FlagsByName(app.Flags))
+	sort.Sort(cli.CommandsByName(app.Commands))
 
 	err := app.Run(os.Args)
 	if err != nil {
@@ -58,25 +84,42 @@ func main() {
 
 func connectAction(c *cli.Context) error {
 	hostname := c.Args().First()
-	ip, err := parseHostname(hostname)
-	if err == nil {
-		return sshConnect(ip)
+	instances, err := findInstances(c, hostname)
+	if err != nil && c.Bool("transparant") {
+		return sshConnect(c, hostname)
 	}
-	instances := queryAws(hostname, "us-east-1")
-	if len(instances) == 0 {
-		fmt.Println("No instances found")
+	var instance Instance
+	if len(instances) > 1 {
+		printTable(instances)
+		instance = getTableChoice(instances)
+	} else {
+		instance = instances[0]
+	}
+	return sshConnect(c, instance.ip)
+}
+func execAction(c *cli.Context) error {
+	hostname := c.Args().First()
+	command := c.Args().Get(1)
+	instances, _ := findInstances(c, hostname)
+	ips := getInstanceIps(instances)
 
-		if c.Bool("transparant") {
-			sshConnect(hostname)
-		}
-		return nil
-	} else if len(instances) == 1 {
-		sshConnect(instances[0].ip)
-		return nil
+	parallelCmd := []string{
+		"parallel",
+		"--no-notice",
+		"--tagstring",
+		"{}",
+		fmt.Sprintf("ssh {} %s", command),
+		":::",
 	}
-	printTable(instances)
-	instance := getTableChoice(instances)
-	sshConnect(instance.ip)
+	parallelCmd = append(parallelCmd, ips...)
+	//fmt.Println(strings.Join(parallelCmd, " "))
+
+	cmd := exec.Command(parallelCmd[0], parallelCmd[1:]...)
+	cmd.Stdout = os.Stdout
+	cmd.Stdin = os.Stdin
+	cmd.Stderr = os.Stderr
+	cmd.Run()
+
 	return nil
 }
 
@@ -87,24 +130,44 @@ func listAction(c *cli.Context) error {
 	return nil
 }
 
+// Find the hostname's ip
+func findInstances(c *cli.Context, hostname string) ([]Instance, error) {
+	var err error
+	ip, err := parseHostname(hostname)
+	if err == nil {
+		fmt.Printf("%v", "sdfsdf")
+		return []Instance{Instance{ip: ip}}, err
+	}
+	instances := queryAws(hostname, c.String("aws-region"))
+	if len(instances) == 0 {
+		fmt.Println("No instances found")
+		return nil, errors.New("No instance found")
+	}
+	return instances, err
+}
+
 // Check if the hostname is a known aws hostname format `ip-123-123-123-123` or just a plain ip.
 func parseHostname(hostname string) (string, error) {
-	if net.ParseIP(hostname) != nil {
-		return hostname, nil
-	}
 	var err error
+	if net.ParseIP(hostname) != nil {
+		return hostname, err
+	}
 	r, _ := regexp.Compile("ip-([0-9]{1,3})-([0-9]{1,3})-([0-9]{1,3})-([0-9]{1,3})")
 	m := r.FindAllStringSubmatch(hostname, 4)
 
 	if len(m) != 1 {
-		return "", errors.New("")
+		return "", errors.New("Unknown hostname")
 	}
 
 	return fmt.Sprintf("%s.%s.%s.%s", m[0][1], m[0][2], m[0][3], m[0][4]), err
 }
 
 // Open an interactive ssh connection
-func sshConnect(hostname string) error {
+func sshConnect(c *cli.Context, hostname string) error {
+	if c.Bool("print") {
+		fmt.Printf("ssh %s", hostname)
+		return nil
+	}
 	fmt.Printf("Connecting to %s...\n", hostname)
 	cmd := exec.Command("ssh", hostname)
 	cmd.Stdout = os.Stdout
@@ -198,4 +261,12 @@ func getTableChoice(instances []Instance) Instance {
 type Instance struct {
 	name string
 	ip   string
+}
+
+func getInstanceIps(instances []Instance) []string {
+	vsm := make([]string, len(instances))
+	for i, v := range instances {
+		vsm[i] = v.ip
+	}
+	return vsm
 }
